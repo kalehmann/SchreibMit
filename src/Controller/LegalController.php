6a9caa790bf29e02ae3506c897ea2a8534dd2bda
Karsten Lehmann <mail@kalehmann.de>
2020-04-04 15:34:25 +0200

    feat: Templates für Datenschutz und Impressum hinzugefügt

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class LegalController
+ * @package App\Controller
+ */
+class LegalController extends AbstractController
+{
+    /**
+     * @Route("/impressum", name="legal_impressum")
+     *
+     * @return Response
+     */
+    public function impressumAction(): Response
+    {
+        return $this->render('legal/impressum.html.twig');
+    }
+
+    /**
+     * @Route("/datenschutz", name="legal_gdpr")
+     *
+     * @return Response
+     */
+    public function gdprAction(): Response
+    {
+        return $this->render('legal/gdpr.html.twig');
+    }
+}
