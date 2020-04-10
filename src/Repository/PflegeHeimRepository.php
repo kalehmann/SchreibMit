c144406522f1cbd6fc0b9ed12cd567cbb74f985e
Karsten Lehmann <mail@kalehmann.de>
2020-04-10 17:40:07 +0200

    feat: Die Maximale Zahl an Kontakten eiens Pflegeheims beachten

--- src/Repository/PflegeHeimRepository.php
+++ src/Repository/PflegeHeimRepository.php
@@ -2,9 +2,11 @@
 
 namespace DrkDD\SchreibMit\Repository;
 
+use Doctrine\ORM\Query\Expr\Join;
 use DrkDD\SchreibMit\Entity\Pflegeheim;
 use DrkDD\SchreibMit\Entity\PostalCode;
 use Doctrine\ORM\AbstractQuery;
+use DrkDD\SchreibMit\Entity\User;
 
 /**
  * Class PflegeHeimRepository
@@ -27,6 +29,9 @@ class PflegeHeimRepository extends \Doctrine\ORM\EntityRepository
         )
             ->addSelect()
             ->from(Pflegeheim::class, 'p')
+            ->leftJoin(User::class, 'u', Join::WITH, $qb->expr()->eq('p.id', 'u.pflegeheim'))
+            ->groupBy('p.id')
+            ->having($qb->expr()->lt($qb->expr()->count('u.id'), 'p.maxContacts'))
             ->setParameter('postal_lat', $postalCode->getLatitude())
             ->setParameter('postal_lon', $postalCode->getLongitude())
             ->orderBy('dist')
